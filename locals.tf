3b4e05fafae28a21a317733961ad3a7e249b16b2
shemaikuzwe <eshemaikuzwe@gmail.com>
2026-04-05 00:18:27 +0200

    setup  github repo

--- locals.tf
+++ locals.tf
@@ -1,15 +1,21 @@
 locals {
-  projects  = {
-    "fem-eci-project"={
-        description = "Example description of project"
+  projects = {
+    "fem-eci-project" = {
+      description = "Example description of project"
     }
   }
   workspace = {
-    "fem-eci-tfe"={
-        description = "fem-eci-workspace"
-        execution_mode = "remote"
-        project_id=module.project["fem-eci-project"].id
-        vcs_repo_identifier="shemaikuzwe/fem-eci-terraform-tfe"
+    "fem-eci-tfe" = {
+      description         = "fem-eci-workspace"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+    "fem-eci-github" = {
+      description         = "Example automation workspace for GitHub resources."
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
