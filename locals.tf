52894a297c6e762c8b2889e03f26adf75661bf5e
Ikuzwe shema Elie <130182097+shemaikuzwe@users.noreply.github.com>
2026-04-05 02:08:29 +0200

    added aws workspaces (#1)
    * added aws workspaces
    
    * fix tf error

--- locals.tf
+++ locals.tf
@@ -1,21 +1,79 @@
 locals {
-  projects = {
+  project = {
     "fem-eci-project" = {
       description = "Example description of project"
     }
   }
+
   workspace = {
     "fem-eci-tfe" = {
-      description         = "fem-eci-workspace"
+      description         = "Example automation workspace for Terraform Cloud resources."
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
+
     "fem-eci-github" = {
       description         = "Example automation workspace for GitHub resources."
-      execution_mode      = "remote"
+      execution_mode      = "local"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
+
+    "fem-eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-east-1a", "us-east-1b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+
+    "fem-eci-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "ikuzwe.dev"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-ikuzwe"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci"
+        },
+      ]
     }
   }
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "project" {
-  for_each = local.projects
+  for_each = local.project
   source  = "ALT-F4-LLC/project/tfe"
   version = "0.4.0"
  
@@ -12,12 +12,14 @@ module "workspace" {
   for_each = local.workspace
   source  = "ALT-F4-LLC/workspace/tfe"
   version = "0.6.0"
- 
+  
   description       = each.value.description
   execution_mode    = each.value.execution_mode
   name              = each.key
   organization_name = var.organization_name
   project_id        = each.value.project_id
+  variables         = try(each.value.variables, [])
+  
   vcs_repo = {
     github_app_installation_id = data.tfe_github_app_installation.this.id
     identifier = each.value.vcs_repo_identifier
