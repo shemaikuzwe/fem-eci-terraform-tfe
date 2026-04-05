8b49884e3c7141a3c97d7bd0243c715d7863aa92
shemaikuzwe <eshemaikuzwe@gmail.com>
2026-04-05 02:04:09 +0200

    added aws workspaces

--- main.tf
+++ main.tf
@@ -12,12 +12,14 @@ module "workspace" {
   for_each = local.workspace
   source  = "ALT-F4-LLC/workspace/tfe"
   version = "0.6.0"
- 
+  
   description       = each.value.description
   execution_mode    = each.value.execution_mode
   name              = each.key
   organization_name = var.organization_name
   project_id        = each.value.project_id
+  variables         = try(each.value.variables, [])
+  
   vcs_repo = {
     github_app_installation_id = data.tfe_github_app_installation.this.id
     identifier = each.value.vcs_repo_identifier
