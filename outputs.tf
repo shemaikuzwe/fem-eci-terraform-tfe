24600d172edb9415e4d8f04037ab435f82b0bbd8
Ikuzwe shema Elie <130182097+shemaikuzwe@users.noreply.github.com>
2026-04-09 17:33:53 +0200

    Feature/outputs (#5)
    * add service workspace
    
    * final version
    
    * add mising outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,21 @@ output "cluster" {
   sensitive = false
   value     = local.cluster
 }
+output "tfe_organization" {
+  sensitive = false
+  value     = var.organization_name
+}
+output "network" {
+  sensitive = false
+  value     = local.network
+}
+
+output "environments" {
+  sensitive = false
+  value     = local.environments
+}
+
+output "services" {
+  sensitive = false
+  value     = local.services
+}
